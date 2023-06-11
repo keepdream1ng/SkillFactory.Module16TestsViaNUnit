f9f4145fe79bdfebe829185af211920c0f1d2b16
Anatolii Ivanov <gotnickname@gmail.com>
2023-06-11 11:39:00 +0600

    added Substruction method UnitTest

--- SkillFactory.Module16TestsViaNUnit.Tests/CalculatorTests.cs
+++ SkillFactory.Module16TestsViaNUnit.Tests/CalculatorTests.cs
@@ -36,17 +36,26 @@ namespace SkillFactory.Module16TestsViaNUnit.Tests
             Assert.That(actual, Is.EqualTo(expected));
         }
 
-        //[Test]
-        //[TestCase( 1, 2, 3)]
-        //[TestCase( 1, -2, -1)]
-        //[TestCase( -1, 2, 1)]
-        //// Overflow scenario.
-        //[TestCase( int.MaxValue, 1, int.MinValue)]
-        //public void AdditionalMustReturnCorrectValue(int a, int b, int expected)
-        //{
-        //    Calculator testCalculator = new Calculator();
-        //    int actual = testCalculator.Additional(a, b);
-        //    Assert.That(actual, Is.EqualTo(expected));
-        //}
+        [Test]
+        [TestCase(2, 1, 1)]
+        [TestCase(1, 2, -1)]
+        [TestCase(0, 0, 0)]
+        [TestCase(5, 0, 5)]
+        [TestCase(10, 5, 5)]
+        [TestCase(0, 5, -5)]
+        [TestCase(5, -5, 10)]
+        [TestCase(int.MaxValue, 1, int.MaxValue - 1)]
+        [TestCase(int.MinValue, -1, int.MinValue + 1)]
+        [TestCase(int.MaxValue, int.MinValue, -1)]
+        [TestCase(int.MinValue, int.MinValue, 0)]
+        [TestCase(20, 10, 10)]
+        [TestCase(-10, -10, 0)]
+        [TestCase(int.MaxValue, int.MaxValue, 0)]
+        public void SubtractionMustReturnCorrectValue(int a, int b, int expected)
+        {
+            Calculator testCalculator = new Calculator();
+            int actual = testCalculator.Subtraction(a, b);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
